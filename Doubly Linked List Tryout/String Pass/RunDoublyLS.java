a8090b5b5b9f36bff13dae9675ebe399deb9db50
vivekVells <techengineervivek@gmail.com>
2017-09-22 02:04:00 -0400

    Create RunDoublyLS.java
    Create Workers
    Workers who use the instrument

---
+++ Doubly Linked List Tryout/String Pass/RunDoublyLS.java
@@ -0,0 +1,13 @@
+public class RunDoublyLS {
+
+	public static void main(String[] args) {
+		MyDoublyLinkedList dllObj1 = new MyDoublyLinkedList();
+		dllObj1.displayAll();
+		dllObj1.addNodeAtBack("Vivek");
+		dllObj1.addNodeAtBack("Iavshali");
+		dllObj1.addNodeAtBack("Sharan");
+		dllObj1.addNodeAtBack("Shalini");
+		dllObj1.displayAll();
+	}
+
+}
