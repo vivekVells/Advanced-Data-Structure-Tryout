c2787ccae35a28dda3f63f0a5a7c6bfa681fd8e1
Vivek Vellaiyappan <techengineervivek@gmail.com>
2017-09-24 04:22:43 -0400

    tools + frame designed

---
+++ Singly Linked List Tryout/Complete Typical Function - Working/NodeParts.java
@@ -0,0 +1,20 @@
+/*
+ * Question:
+ * Create a generic Singly Linked List that enables the user to
+ *  CHECK node count status
+ *  INSERT node at the START 
+ *  INSERT node at the END
+ *  REMOVE node at the START
+ *  REMOVE node at the END
+ *  DISPLAY all nodes
+ *  
+ *  Refer "MainSinglyWorker.java" file for output
+ */
+
+//CODE:
+package ADS.Chap3;
+
+public class NodeParts {
+	protected String nodeHeart;
+	protected NodeParts nodeNext;
+}
